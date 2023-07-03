d838e547ca0a82bdd4e6ff2a6f91bd10f5083240
MateuszKar2 <system505@protonmail.com>
2023-07-03 21:36:11 +0200

    add file

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,66 +4,68 @@ import { v4 as uuid } from 'uuid';
 import styles from './ContactForm.module.css';
 
 class ContactForm extends React.Component {
-    static PropTypes= {
-        onSubmit: PropTypes.func.isRequired,
+    static propTypes = {
+      onSubmit: PropTypes.func.isRequired,
     };
-
-state = {
-    name: '',
-    number: '',
-};
-
-handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value});
-};
-
-handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit({...this.state, id: uuid() });
-    this.setState({ name: '', number: ''})
-}
-
-render() {
-    const { name, number } = this.state;
-    <form className={styles.form} onSubmit={handleSubmit}>
-        <div>
+  
+    state = {
+      name: '',
+      number: '',
+    };
+  
+    handleChange = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+  
+    handleSubmit = e => {
+      e.preventDefault();
+      this.props.onSubmit({ ...this.state, id: uuid() });
+      this.setState({ name: '', number: '' });
+    };
+  
+    render() {
+      const { name, number } = this.state;
+  
+      return (
+        <form className={styles.form} onSubmit={this.handleSubmit}>
+          <div>
             <label className={styles.label} htmlFor="name">
-                Name
+              Name
             </label>
             <input
-            className={styles.input}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            placeholder="Name"
-            required
-           />
-        </div>
-        <div>
-            <label className={styles.label} htmlFor="number">
-                Number
-            </label>
-            <input
-            className={styles.input}
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            placeholder="Phone number"
-            required
+              className={styles.input}
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+              placeholder="Name"
+              required
             />
-        </div>
-        <button className={styles.submitBtn} type="submit">
-                Add contact
-        </button>
-    </form>
-    
-}
-}
+          </div>
+          <div>
+            <label className={styles.label} htmlFor="number">
+              Number
+            </label>
+            <input
+              className={styles.input}
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+              placeholder="Phone number"
+              required
+            />
+          </div>
+          <button className={styles.submitBtn} type="submit">
+            Add contact
+          </button>
+        </form>
+      );
+    }
+  }
 
-ContactFrom.propTypes = {
+ContactForm.propTypes = {
     onSubmit: PropTypes.func.isRequired,
-}
+  };
 
 export default ContactForm;
